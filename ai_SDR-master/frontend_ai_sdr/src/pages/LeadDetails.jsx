577234440cffc00f0ce2b2660aa12861140f4cbb
Sreephaneesha <kanugovisreephaneesha@gmail.com>
2025-08-16 02:32:49 +0530

    Add button to generate Jitsi meeting link in LeadDetails component

--- ai_SDR-master/frontend_ai_sdr/src/pages/LeadDetails.jsx
+++ ai_SDR-master/frontend_ai_sdr/src/pages/LeadDetails.jsx
@@ -185,6 +185,21 @@ const LeadDetails = () => {
                         <div className="mt-10">
                             <h3 className="text-lg font-semibold mb-4">Add Meeting Details</h3>
                             <Form layout="vertical" onFinish={handleUpdateMeetingLinkAndDate}>
+                                <Button
+                                    type="default"
+                                    style={{ marginBottom: "1rem" }}
+                                    onClick={() => {
+                                        if (!lead?._id) {
+                                            message.error("Lead ID is missing!");
+                                            return;
+                                        }
+                                        const link = `https://meet.jit.si/${lead._id}-${Date.now()}`;
+                                        setMeetingLink(link);
+                                        message.success("Jitsi meeting link generated!");
+                                    }}
+                                >
+                                    Generate Jitsi Link
+                                </Button>
                                 <Form.Item label="Meeting Link" required>
                                     <Input
                                         placeholder="Enter meeting link"
